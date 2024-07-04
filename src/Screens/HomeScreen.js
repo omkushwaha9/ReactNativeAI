f40816ae2947f7d672a667db2d6baef337d02b56
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-04 23:35:29 +0530

    Chatbox

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,12 +1,14 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Features from '../components/Features';
+import { dummyMessages } from '../constaints';
+
 
 export default function HomeScreen() {
-    const [messages, setMessages] = useState([]);
+    const [messages, setMessages] = useState(dummyMessages);
   return (
     <View style={styles.header1}>
       <SafeAreaView style={styles.header2}>
@@ -20,14 +22,70 @@ export default function HomeScreen() {
        {/* features \\ messages */}
        {
         messages.length>0? (
-            <View>
+            <View style={styles.class2}>
+               <Text style={styles.Text1}>
+                 Assistant
+               </Text>
+               <View style={styles.class3}>
 
+               </View>
             </View>
         ) : (
             <Features/>
         )
        }
       </SafeAreaView>
+      <ScrollView
+         bounces={false}
+         style={styles.scview}
+         showsVerticalScrollIndicator={false}
+      >
+        {
+            messages.map((messages, index)=> {
+                if (messages.role=='assistant'){
+                    if (messages.content.includes('https')){
+                        // its an ai image
+                        return (
+                            <View key={index} style={styles.class4}>
+                          <View style={styles.class7}>
+                             <Image
+                             source={{uri: messages.content}}
+                             style={styles.Image1}
+                             />
+                          </View>
+                        </View>
+                        )
+                    } else {
+                        //Text response
+                        return (
+                            <View key={index} >
+                              <View style={styles.class6}>
+                                <Text style={styles.Text2}>
+                                    {messages.content}
+                                </Text>
+                              </View>
+                            </View>
+                        )
+                    }
+                } else {
+                    // userinput
+                    return (
+                        <View key={index} style={styles.class4}>
+                          <View style={styles.class5}>
+                            <Text style={styles.Text2}>
+                                {messages.content}
+                            </Text>
+                          </View>
+                        </View>
+                    )
+                }
+                
+            })
+        }
+
+      </ScrollView>
+
+
     </View>
   )
 }
@@ -49,5 +107,60 @@ const styles = StyleSheet.create({
         height: hp(15),
         width: hp(15),
 
+    },
+    class2:{
+         marginHorizontal:10,
+         flex:1,
+    },
+    Text1:{
+        color:'grey',
+        fontWeight:'600',
+        margin:1,
+        fontSize:wp(5),
+    },
+    class3:{
+        height:hp(58),
+        backgroundColor:"lightgrey",
+        borderRadius:27,
+        
+    },
+    scview:{
+        margintop: 4,
+        
+    },
+    Text2:{
+        fontSize:17,
+        
+    },
+    class4:{
+       flexDirection:'row',
+       justifyContent:'flex-end',
+    },
+    class5:{
+       
+        backgroundColor:'white',
+        borderRadius:9,
+        padding:7,
+        borderTopRightRadius:'none'
+    },
+    class6:{
+        
+        backgroundColor:'lightgreen',
+        borderRadius:9,
+        padding:7,
+        borderTopLeftRadius:'none'
+    },
+    class7:{
+        
+        backgroundColor:'lightgreen',
+        borderRadius:9,
+        padding:2,
+        borderTopLeftRadius:'none'
+    },
+    Image1:{
+        borderRadius:8,
+        resizeMode:'contain',
+        height:wp(60),
+        width:wp(60),
     },
 })
---
+++ src/constaints/index.js
@@ -0,0 +1,20 @@
+export const dummyMessages = [
+    {
+        role: 'user',
+        content: 'How are you?'
+
+    },
+    {
+        role: 'assistant',
+        content: "I'm fine, How may i help you today"
+    },
+    {
+        role: 'user',
+        content: 'create an image of a dog playing with cat'
+    },
+    {
+        role: 'assistant',
+        content: 'https://www.shutterstock.com/image-photo/happy-mixed-breed-dog-portrait-600nw-1663393486.jpg'
+    },
+
+]
